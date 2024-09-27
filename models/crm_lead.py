2033a774cd08d8cb6720c13a17760d19509efce8
Muhammed Anas <muhammadanaswayanad@gmail.com>
2024-09-27 12:47:19 +0530

    gpt

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -3,17 +3,17 @@ from odoo import models, fields
 class CRMLead(models.Model):
     _inherit = 'crm.lead'
 
-    x_date_of_birth = fields.Date(string="Date of Birth")
-    x_age = fields.Integer(string="Age")
+    x_date_of_birth = fields.Char(string="Date of Birth")
+    x_age = fields.Char(string="Age")
     x_father_guardian = fields.Char(string="Father/Guardian")
     x_qualification = fields.Char(string="Qualification")
     x_course = fields.Char(string="Course")
-    x_permanent_address = fields.Text(string="Permanent Address")
+    x_permanent_address = fields.Char(string="Permanent Address")
     x_city_town = fields.Char(string="City/Town")
     x_state = fields.Char(string="State")
     x_district = fields.Char(string="District")
     x_mobile_alt = fields.Char(string="Mobile (Alt)")
     x_enrollment_number = fields.Char(string="Enrollment No")
     x_receipt_number = fields.Char(string="Receipt No")
-    x_fees_paid = fields.Float(string="Fees Paid")
-    x_balance_due = fields.Float(string="Balance Due")
+    x_fees_paid = fields.Char(string="Fees Paid")
+    x_balance_due = fields.Char(string="Balance Due")
