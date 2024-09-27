c05e22ad1927e672bc488799b6fc4a4705e7edc1
Muhammed Anas <muhammadanaswayanad@gmail.com>
2024-09-27 12:17:55 +0530

    Update __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Custom CRM Lead Fields',
+    'version': '1.0',
+    'depends': ['crm', 'product'],
+    'category': 'CRM',
+    'description': """
+    Adds custom fields to CRM Lead model.
+    """,
+    'author': 'Muhammed Anas',
+    'data': [
+        'views/crm_lead_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
