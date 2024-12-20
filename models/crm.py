12b14c44143d858185d9108a20c7a30154b2075a
Muhammed Anas <muhammadanaswayanad@gmail.com>
2024-12-20 11:57:53 +0530

    exp 3

--- models/crm.py
+++ models/crm.py
@@ -137,16 +137,18 @@ class CRMLead(models.Model):
         if not self.env.context.get('importing_leads'):
             for record in res:
                 record.set_lead_queue()
-                record._check_course_id_required()
-                record._check_source_id_required()
+                if record.type == 'opportunity':
+                    record._check_course_id_required()
+                    record._check_source_id_required()
         return res
     
     def write(self, vals):
         res = super(CRMLead, self).write(vals)
         if not self.env.context.get('importing_leads'):
             self.set_lead_queue()
-            self._check_course_id_required()
-            self._check_source_id_required()
+            if self.type == 'opportunity':
+                self._check_course_id_required()
+                self._check_source_id_required()
         return res
 
     def set_lead_queue(self):
