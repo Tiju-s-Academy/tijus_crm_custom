b4336c678cb98259f0f7781d0e1574c4d8cba1d5
Muhammed Anas <muhammadanaswayanad@gmail.com>
2025-03-01 11:38:59 +0530

    feat: remove crm_lead.py from manifest dependencies

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,6 @@
         'security/ir.model.access.csv',
         'security/record_rules.xml',
         'views/res_partner.xml',
-        'models/crm_lead.py',  # Add this line
         'views/crm_lead_views.xml',
         'views/crm_views.xml',
     ],
