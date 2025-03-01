309c30f8f978ab8519bdf4b1efb8a1ae2eb6dd1c
Muhammed Anas <muhammadanaswayanad@gmail.com>
2025-03-01 11:46:10 +0530

    feat: enhance date_closed field editability with dynamic readonly status and toggle functionality

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -6,11 +6,26 @@ class CrmLead(models.Model):
     date_closed_editable = fields.Boolean(
         string='Date Closed Editable',
         default=False,
-        tracking=True
+        tracking=True,
+        copy=False
     )
 
-    def action_toggle_date_closed_edit(self):
-        """Toggle editability of date_closed field"""
+    def write(self, vals):
+        # Override write to handle date_closed field editability
+        if 'date_closed' in vals and not self.date_closed_editable:
+            vals.pop('date_closed')
+        return super(CrmLead, self).write(vals)
+
+    @api.model
+    def fields_get(self, allfields=None, attributes=None):
+        # Override to dynamically set readonly status
+        res = super(CrmLead, self).fields_get(allfields, attributes)
+        if 'date_closed' in res:
+            res['date_closed']['readonly'] = False
+        return res
+
+    def toggle_date_closed(self):
+        """Enable/disable date_closed field editability"""
         for record in self:
             record.write({'date_closed_editable': not record.date_closed_editable})
         return True
