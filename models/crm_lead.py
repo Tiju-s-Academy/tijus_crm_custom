b6d40c49526c62be9a833bde20f19875cc0daf50
Muhammed Anas <muhammadanaswayanad@gmail.com>
2025-03-01 11:43:26 +0530

    feat: improve date_closed edit functionality with enhanced toggle buttons and dynamic readonly attributes

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -1,25 +1,15 @@
-from odoo import models, fields, api, _
+from odoo import models, fields, api
 
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
-    date_closed_editable = fields.Boolean('Date Closed Editable', default=False)
-
-    @api.model
-    def fields_get(self, allfields=None, attributes=None):
-        """Override to dynamically set readonly status of date_closed"""
-        res = super(CrmLead, self).fields_get(allfields, attributes)
-        if 'date_closed' in res:
-            res['date_closed']['readonly'] = not self.date_closed_editable
-        return res
+    date_closed_editable = fields.Boolean(
+        string='Date Closed Editable',
+        default=False,
+        tracking=True
+    )
 
     def toggle_date_closed_edit(self):
-        """Enable/disable date_closed field editability"""
+        """Toggle editability of date_closed field"""
         for record in self:
-            record.write({
-                'date_closed_editable': not record.date_closed_editable
-            })
-        return {
-            'type': 'ir.actions.client',
-            'tag': 'reload',
-        }
+            record.date_closed_editable = not record.date_closed_editable
